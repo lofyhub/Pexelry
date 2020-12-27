aef273ccd6640ce340c7a5f36b7e5095c8f9d34c
John Philip <developerphilo@gmail.com>
2020-12-27 16:28:21 +0300

    updated changes
    fixed the menu bar

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,187 +1,247 @@
 <template>
-  <div class="footercomp">
-    <div class="footer">
-      <div class="learn">
-        <h3>Learn</h3>
-        <ul>
-          <li>
-            <a href="http://" target="_blank" rel="noopener noreferrer"
-              >PPC Marketing</a
-            >
-          </li>
-          <li>
-            <a href="http://" target="_blank" rel="noopener noreferrer">SEO</a>
-          </li>
-          <li>
-            <a href="http://" target="_blank" rel="noopener noreferrer"
-              >UX/UI</a
-            >
-          </li>
-          <li>
-            <a href="http://" target="_blank" rel="noopener noreferrer"
-              >Web Design</a
-            >
-          </li>
-          <li>
-            <a href="http://" target="_blank" rel="noopener noreferrer"
-              >Social Media</a
-            >
-          </li>
-          <li>
-            <a href="http://" target="_blank" rel="noopener noreferrer"
-              >Case Studies</a
-            >
-          </li>
-        </ul>
-      </div>
-      <div class="product">
-        <h3>Product</h3>
+  <div class="footer">
+    <footer class="footer-distributed">
+      <div class="footer-left">
+        <h3>Pexelry<span>logo</span></h3>
 
-        <ul>
-          <li><a href="">FAQ</a></li>
-          <li><a href="">Features</a></li>
-          <li><a href="">Support</a></li>
-        </ul>
+        <p class="footer-links">
+          <a href="#" class="link-1">Home</a>
+
+          <a href="#">Blog</a>
+
+          <a href="#">Pricing</a>
+
+          <a href="#">About</a>
+
+          <a href="#">Faq</a>
+
+          <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Pexelry © 2020</p>
       </div>
-      <div class="company">
-        <h3>Company</h3>
-        <ul>
-          <li><a href="">Contact Us</a></li>
-          <li><a href="">About Us</a></li>
-          <li><a href="">Our Team</a></li>
-          <li><a href="http://">Investors</a></li>
-        </ul>
-      </div>
-      <div class="newsletter">
-        <h3>Join Our newsletter</h3>
-        <div class="news">
-          <form>
-            <input
-              type="email"
-              value=""
-              required
-              placeholder="Add your email here"
-            />
-            <span><button>SIGN UP</button></span> <br />
-            <span class="span">Get exclusive deals and more!</span>
-          </form>
+
+      <div class="footer-center">
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
         </div>
-        <div class="connect">
-          <h3>Connect with us</h3>
-          <div class="social">
-            <ul>
-              <li>
-                <a href="http://" target="_blank" rel="noopener noreferrer"
-                  ><font-awesome-icon :icon="['fab', 'twitter']"
-                /></a>
-              </li>
-              <li>
-                <a href="http://" target="_blank" rel="noopener noreferrer"
-                  ><font-awesome-icon :icon="['fab', 'instagram']"
-                /></a>
-              </li>
-              <li>
-                <a href="http://" target="_blank" rel="noopener noreferrer"
-                  ><font-awesome-icon :icon="['fab', 'github']"
-                /></a>
-              </li>
-              <li>
-                <a href="http://" target="_blank" rel="noopener noreferrer"
-                  ><font-awesome-icon :icon="['fab', 'youtube']"
-                /></a>
-              </li>
-              <li>
-                <a href="http://" target="_blank" rel="noopener noreferrer"
-                  ><font-awesome-icon :icon="['fab', 'linkedin']"
-                /></a>
-              </li>
-            </ul>
-          </div>
+
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+1.555.555.5555</p>
+        </div>
+
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="mailto:support@company.com">support@company.com</a></p>
         </div>
       </div>
-    </div>
-    <div class="copyright">
-      Copyright @ 2020 Pexelry "pic it" , All rights Reserved | Terms &
-      Conditions | Private Policy
-    </div>
+
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <span>About the company</span>
+          Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
+          euismod convallis velit, eu auctor lacus vehicula sit amet.
+        </p>
+
+        <div class="footer-icons">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-github"></i></a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
-.footercomp {
-  background-color: lightseagreen;
-  padding-top: 2rem;
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:400,500,300,700);
+
+* {
+  font-family: Open Sans;
 }
-ul {
-  list-style: none;
+
+.footer-distributed {
+  background: #666;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+  box-sizing: border-box;
+  width: 100%;
+  text-align: left;
+  font: bold 16px sans-serif;
+  padding: 55px 50px;
 }
-ul li a {
-  color: #333333;
+
+.footer-distributed .footer-left,
+.footer-distributed .footer-center,
+.footer-distributed .footer-right {
+  display: inline-block;
+  vertical-align: top;
 }
-ul li a:hover {
-  color: #333333;
+
+/* Footer left */
+
+.footer-distributed .footer-left {
+  width: 40%;
+}
+
+/* The company logo */
+
+.footer-distributed h3 {
+  color: #ffffff;
+  font: normal 36px "Open Sans", cursive;
+  margin: 0;
+}
+
+.footer-distributed h3 span {
+  color: lightseagreen;
+}
+
+/* Footer links */
+
+.footer-distributed .footer-links {
+  color: #ffffff;
+  margin: 20px 0 12px;
+  padding: 0;
+}
+
+.footer-distributed .footer-links a {
+  display: inline-block;
+  line-height: 1.8;
+  font-weight: 400;
+  text-decoration: none;
+  color: inherit;
+}
+
+.footer-distributed .footer-company-name {
+  color: #222;
+  font-size: 14px;
+  font-weight: normal;
+  margin: 0;
+}
+
+/* Footer Center */
+
+.footer-distributed .footer-center {
+  width: 35%;
+}
+
+.footer-distributed .footer-center i {
+  background-color: #33383b;
+  color: #ffffff;
+  font-size: 25px;
+  width: 38px;
+  height: 38px;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 42px;
+  margin: 10px 15px;
+  vertical-align: middle;
+}
+
+.footer-distributed .footer-center i.fa-envelope {
+  font-size: 17px;
+  line-height: 38px;
+}
+
+.footer-distributed .footer-center p {
+  display: inline-block;
+  color: #ffffff;
+  font-weight: 400;
+  vertical-align: middle;
+  margin: 0;
+}
+
+.footer-distributed .footer-center p span {
+  display: block;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 2;
+}
+
+.footer-distributed .footer-center p a {
+  color: lightseagreen;
   text-decoration: none;
 }
-ul li {
-  padding: 0.5rem 0;
+
+.footer-distributed .footer-links a:before {
+  content: "|";
+  font-weight: 300;
+  font-size: 20px;
+  left: 0;
+  color: #fff;
+  display: inline-block;
+  padding-right: 5px;
 }
-h3 {
-  font-size: 1.4rem;
+
+.footer-distributed .footer-links .link-1:before {
+  content: none;
+}
+
+/* Footer Right */
+
+.footer-distributed .footer-right {
+  width: 20%;
+}
+
+.footer-distributed .footer-company-about {
+  line-height: 20px;
+  color: #92999f;
+  font-size: 13px;
+  font-weight: normal;
+  margin: 0;
+}
+
+.footer-distributed .footer-company-about span {
+  display: block;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.footer-distributed .footer-icons {
+  margin-top: 25px;
+}
+
+.footer-distributed .footer-icons a {
+  display: inline-block;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  background-color: #33383b;
+  border-radius: 2px;
+
+  font-size: 20px;
+  color: #ffffff;
   text-align: center;
-  padding-bottom: 1.5rem;
+  line-height: 35px;
+
+  margin-right: 3px;
+  margin-bottom: 5px;
 }
-input {
-  padding: 4px 18px;
-  opacity: 0.5;
-  font-style: italic;
-}
-input:focus {
-  outline: none;
-}
-button {
-  padding: 5px 10px;
-  border: none;
-  color: white;
-  background: green;
-  font-size: 16px;
-}
-button:hover {
-  background-color: #28a745;
-}
-button:focus {
-  outline: none;
-}
-.footer {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-}
-.social ul {
-  display: flex;
-  font-size: 21px;
-}
-.social ul li {
-  padding-left: 0.5rem;
-  padding-top: 0;
-}
-.connect h3 {
-  padding: 1rem 0;
-}
-.span {
-  opacity: 0.5;
-  padding-top: 1rem;
-  text-align: center;
-  font-style: italic;
-}
-.copyright {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem 0;
-  font-weight: 500;
-  font-size: 1.25rem;
-  font-style: italic;
+
+/* If you don't want the footer to be responsive, remove these media queries */
+
+@media (max-width: 880px) {
+  .footer-distributed {
+    font: bold 14px sans-serif;
+  }
+
+  .footer-distributed .footer-left,
+  .footer-distributed .footer-center,
+  .footer-distributed .footer-right {
+    display: block;
+    width: 100%;
+    margin-bottom: 40px;
+    text-align: center;
+  }
+
+  .footer-distributed .footer-center i {
+    margin-left: 0;
+  }
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,70 @@
 
 export default {
-    // Target (https://go.nuxtjs.dev/config-target)
+  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'PEXELRY - PIC IT',
-        htmlAttrs: {
+    htmlAttrs: {
       lang: "en",  // it sets the language English
     },
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-            // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
       { hid: 'description', name: 'description', content: 'PEXELRY - PIC IT' },
-  { property: "og:site_name", content: "PEXELRY - PIC IT" },
-  { hid: "og:type", property: "og:type", content: "website" },
-  {
-    hid: "og:url",
-    property: "og:url",
-    content: "https://pexelry.netlify.app",
-  },
-  {
-    hid: "og:title",
-    property: "og:title",
-    content: "PEXELRY - PIC IT",
-  },
-  {
-    hid: "og:description",
-    property: "og:description",
-    content: "PEXELRY - PIC IT",
-  },
-  {
-    hid: "og:image",
-    property: "og:image",
-    content: "https://images.unsplash.com/photo-1591597787791-4b837b0bfa5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-  },
-  { property: "og:image:width", content: "740" },
-  { property: "og:image:height", content: "300" },
-    { name: "twitter:site", content: "@amjohnphilip" },
-  { name: "twitter:card", content: "summary_large_image" },
-  {
-    hid: "twitter:url",
-    name: "twitter:url",
-    content: "https://pexelry.netlify.app",
-  },
-  {
-    hid: "twitter:title",
-    name: "twitter:title",
-    content: "PEXELRY - PIC IT",
-  },
-  {
-    hid: "twitter:description",
-    name: "twitter:description",
-    content: "PEXELRY - PIC IT"
-  },
-  {
-    hid: "twitter:image",
-    name: "twitter:image",
-    content: "https://images.unsplash.com/photo-1591597787791-4b837b0bfa5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-  },
+      { property: "og:site_name", content: "PEXELRY - PIC IT" },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://pexelry.netlify.app",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "PEXELRY - PIC IT",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "PEXELRY - PIC IT",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://images.unsplash.com/photo-1591597787791-4b837b0bfa5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+      },
+      { property: "og:image:width", content: "740" },
+      { property: "og:image:height", content: "300" },
+      { name: "twitter:site", content: "@amjohnphilip" },
+      { name: "twitter:card", content: "summary_large_image" },
+      {
+        hid: "twitter:url",
+        name: "twitter:url",
+        content: "https://pexelry.netlify.app",
+      },
+      {
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "PEXELRY - PIC IT",
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content: "PEXELRY - PIC IT"
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "https://images.unsplash.com/photo-1591597787791-4b837b0bfa5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+      },
 
     ],
     link: [
@@ -73,7 +78,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-     { src: '~/plugins/vue-scroll-indicator', ssr: false }
+    { src: '~/plugins/vue-scroll-indicator', ssr: false }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -81,55 +86,55 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    
-  ],
-  pageTransition:{
-  name:'page',
-  mode:'out-in'
 
-},
+  ],
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in'
+
+  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-        [
+    [
       'nuxt-fontawesome', {
         imports: [
-         {
-           set: '@fortawesome/free-solid-svg-icons',
-           icons: ['fas']
-         },
-         {
-           set:'@fortawesome/free-brands-svg-icons',
-           icons: ['fab']
-         }
-       ]
+          {
+            set: '@fortawesome/free-solid-svg-icons',
+            icons: ['fas']
+          },
+          {
+            set: '@fortawesome/free-brands-svg-icons',
+            icons: ['fab']
+          }
+        ]
       }
-]
+    ]
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-     configureWebpack: {
-    optimization: {
-      runtimeChunk: 'single',
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-              return `npm.${packageName.replace('@', '')}`;
+    configureWebpack: {
+      optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                return `npm.${packageName.replace('@', '')}`;
+              },
             },
           },
         },
       },
     },
-  },
-    analyze:true,
+    analyze: true,
   }
 }
